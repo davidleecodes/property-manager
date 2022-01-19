b560ea9a95c12f98e6a7e5f7f372fcc112211f52
David Lee <concoctionfactory@gmail.com>
2022-01-18 20:07:29 -0500

    Update and rename client/src/pages/SignUp/SignUp.jsx to client/src/pages/Signup/Signup.jsx

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import UserFormOwner from "../../components/forms/UserFormOwner";
 
-export default function SignUp() {
+export default function Signup() {
   return (
     <Container component="main" maxWidth="xs">
       <Grid container direction="column" sx={{ mt: 2 }} spacing={2}>
